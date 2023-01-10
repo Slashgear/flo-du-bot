a036507785473de482b04519f03a6b817916df32
Antoine Caron <antoine.caron@bedrockstreaming.com>
2023-01-10 13:13:07 +0100

    fix usage of API

--- src/octokitClient.ts
+++ src/octokitClient.ts
@@ -1,3 +1,3 @@
 import { getInput } from "@actions/core";
-import GithubActionUtils from "@actions/github";
-export const github = GithubActionUtils.getOctokit(getInput("token"));
+import { getOctokit } from "@actions/github";
+export const github = getOctokit(getInput("token"));
